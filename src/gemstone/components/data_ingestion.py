9c15d22ef4e0a278141e4d4ad4e5c9b01e03166d
vaibhavthapli <hellovaibt@gmail.com>
2025-02-12 13:35:26 +0530

    ingestion

--- src/gemstone/components/data_ingestion.py
+++ src/gemstone/components/data_ingestion.py
@@ -40,7 +40,7 @@ class DataIngestion:
             logging.info(f"Raw data is saved at {self.ingestion_config.raw_data_path}")
             #train test
             logging.info("Train test split initiated")
-            train_set,test_set = train_test_split(df,test_size=0.1,random_state=42)
+            train_set,test_set = train_test_split(df,test_size=0.01,random_state=42)
 
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
